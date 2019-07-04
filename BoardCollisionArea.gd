f0c80a5bd471d787f832c7ccb230c5af9d91dca3
BadAssBlueRhino <51770465+BadAssBlueRhino@users.noreply.github.com>
2019-07-03 20:17:47 -0600

    Update BoardCollisionArea.gd

--- BoardCollisionArea.gd
+++ BoardCollisionArea.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 signal entered_node
-signal left_node
 
 var is_start_position : bool = false setget _set_is_start_position, _get_is_start_position
 var is_occupied : bool = false setget _set_is_occupied, _get_is_start_position
+
+func _ready():
+	connect("entered_node", get_parent(), "entered_node_bounce")
 
 func _set_is_start_position(switch):
 	is_start_position = switch
@@ -19,15 +21,15 @@ func _get_is_occupied():
 	return is_occupied
 
 func _on_Board_Area_mouse_entered():
-	# Set location relative to canvas, because we alter its original positon
-	get_node("/root/GlobalStorage")._set_drop_location(get_global_transform_with_canvas().get_origin())
-	get_node("/root/GlobalStorage")._set_alternate_drop_location(true)
-	get_node("/root/GlobalStorage")._set_global_node_entered_name(self.get_name())
-	emit_signal("entered_node", self.name, self.get_global_transform_with_canvas().get_origin())
+	emit_signal("entered_node", true, self.name, self.get_global_transform_with_canvas().get_origin(), is_occupied)
 	print("Mouse in.") # Debug log.
 
 func _on_Board_Area_mouse_exited():
 	get_node("/root/GlobalStorage")._set_alternate_drop_location(false)
 	get_node("/root/GlobalStorage")._set_global_node_entered_name("empty")
-	emit_signal("left_node")
+	emit_signal("entered_node", false, self.name, self.get_global_transform_with_canvas().get_origin(), is_occupied)
 	print("Mouse out.") # Debug log.
+
+func _is_now_occupied(node)
+	if self.name() == node
+		is_occupied = true
